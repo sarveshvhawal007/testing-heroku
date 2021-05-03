6f0c737c1203f971e3c4ac604eaafdb0ac3044cf
sarveshvhawal007 <2018csb1121@iitpr.ac.in>
2021-05-03 12:37:44 +0530

    t

--- blindRead/settings.py
+++ blindRead/settings.py
@@ -169,7 +169,7 @@ STATIC_URL = '/static/'
 #     ]
 # else:
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
